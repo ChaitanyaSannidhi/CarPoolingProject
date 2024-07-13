0c8cf33e62bb4422d206f33e71f4e66109c988b4
sriramya <sriramyavijjana@gmail.com>
2024-07-13 14:08:57 +0530

    added sign up with google

--- app/src/main/java/com/example/carpooling/SignupActivity.java
+++ app/src/main/java/com/example/carpooling/SignupActivity.java
@@ -1,4 +1,6 @@
 package com.example.carpooling;
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -21,10 +23,12 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -61,6 +65,20 @@ public class SignupActivity extends AppCompatActivity {
         preferenceManager = new PreferenceManager(this);
 
         Button signupButton = findViewById(R.id.button2);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id)) // Replace with your web client ID
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        ImageView signinwithgoogle = findViewById(R.id.signgoogle);
+        signinwithgoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SignInWithGoogle();
+            }
+        });
+
         signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -127,6 +145,110 @@ public class SignupActivity extends AppCompatActivity {
             }
         });
     }
+    private void SignInWithGoogle() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign-In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                // Google Sign-In failed, handle this scenario
+                Log.w(TAG, "Google sign in failed", e);
+                Toast.makeText(SignupActivity.this, "Google sign in failed", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            if (user != null) {
+                                // Check if user is new or existing and handle accordingly
+                                boolean isNewUser = task.getResult().getAdditionalUserInfo().isNewUser();
+                                if (isNewUser) {
+                                    // Handle new user registration in Firestore
+                                    String username = user.getDisplayName(); // Example: Use username from Google
+                                    String email = user.getEmail(); // Example: Use email from Google
+                                    createUserGoogleAccount(email, username);
+                                } else {
+                                    // Existing user, navigate to home activity
+                                    navigateToHomeActivity();
+                                }
+                            }
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(SignupActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+    private void createUserGoogleAccount(String email, String username) {
+        registerButton.setEnabled(false); // Disable button during account creation
+
+        if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(username)) {
+            FirebaseUser currentUser = mAuth.getCurrentUser();
+
+            if (currentUser != null) {
+                final String currentUserId = currentUser.getUid();
+                DocumentReference userDocRef = db.collection("GoogleUsers").document(currentUserId);
+
+                // Create a user map
+                Map<String, Object> userObj = new HashMap<>();
+                userObj.put("email", email);
+                userObj.put("username", username);
+                // Add more fields as needed
+
+                // Add the user to Firestore
+                userDocRef.set(userObj)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                // Document added successfully
+                                Toast.makeText(SignupActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                                navigateToHomeActivity();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Error adding document
+                                Toast.makeText(SignupActivity.this, "Error adding document: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                registerButton.setEnabled(true); // Enable button again for retry
+                            }
+                        });
+            } else {
+                Toast.makeText(SignupActivity.this, "Current user is null", Toast.LENGTH_SHORT).show();
+                registerButton.setEnabled(true); // Enable button again for retry
+            }
+        } else {
+            Toast.makeText(SignupActivity.this, "Email or Username is empty", Toast.LENGTH_SHORT).show();
+            registerButton.setEnabled(true); // Enable button again for retry
+        }
+    }
+
+    private void navigateToHomeActivity() {
+        // Navigate to HomeActivity after successful sign-in
+        preferenceManager.setLoggedIn(true);
+        startActivity(new Intent(SignupActivity.this, HomeActivity.class));
+        finish();
+    }
+
 
     private void createUserEmailAccount(String email, String password, final String username) {
         registerButton.setEnabled(false);
